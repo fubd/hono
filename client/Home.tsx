f40a96b1f8a2ca79ff9a0a4ef8d831b16421c777
fubd <312bdf@gmail.com>
2025-08-22 23:30:20 +0800

    init

--- client/Home.tsx
+++ client/Home.tsx
@@ -1,6 +1,6 @@
 function Animal() {
   return (
-    <div style={{boxSizing: 'border-box', width: 300, height: 300, overflowY: 'auto'}}>
+    <div style={{ boxSizing: 'border-box', width: 500, height: 500, overflowY: 'auto' }}>
       <div
         style={{
           boxSizing: 'border-box',
@@ -11,7 +11,10 @@ function Animal() {
           background: 'powderblue',
         }}
       ></div>
-      <div style={{boxSizing: 'border-box', border: '1px solid hotpink', width: 100, height: 100, margin: 20, background: '#f0f0f0'}}></div>
+      <div style={{ boxSizing: 'border-box', border: '1px solid hotpink', width: 100, height: 100, margin: 20, background: '#f0f0f0' }}></div>
+      <div style={{ background: 'green', width: 100, height: 100 }}>
+        new
+      </div>
     </div>
   );
 }
